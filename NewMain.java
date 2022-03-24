43a59b72970f86b1a5ac05c6dbab670da23bd5e4
1988029 <1988029@alu.murciaeduca.es>
2022-03-24 20:10:26 +0100

    NOOOO NANA NO PONGAS TILDES EN TU ARCHIVO

--- NewMain.java
+++ NewMain.java
@@ -40,6 +40,38 @@ return this.precio + this.iva * this.precio / 100;
 
 
 
+class ropa extends producto{
+static float iva = 10;
+
+ropa(){
+this.nombre = "Camiseta";
+this.precio = 30;
+}
+void setNombre(String nombre){
+this.nombre = nombre;
+}
+
+void setPrecifloat (float precio){
+this.precio = precio;
+}
+
+String getNombre(){
+return this.nombre;
+}float getPrecio(){
+return this.precio;
+}
+
+public float precioIva(){
+return this.precio + this.iva * this.precio / 100;
+}
+}
+
+
+
+
+
+
+
 
 
 
