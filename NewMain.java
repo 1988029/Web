7e8e99e92d1f44deb698f8dface22a61b6091b9b
1988029 <1988029@alu.murciaeduca.es>
2022-03-24 10:07:14 +0100

    trabajo

---
+++ NewMain.java
@@ -0,0 +1,76 @@
+package com.mycompany.array;
+
+interface vendible{
+public float precioIva();
+}
+
+
+abstract class producto implements vendible{
+    String nombre;
+  float precio;
+}
+
+
+class alimentacion extends producto{
+static float iva = 4;
+
+alimentacion(){
+this.nombre = "Leche";
+this.precio = 10;
+}
+void setNombre(String nombre){
+this.nombre = nombre;
+}
+
+void setPrecifloat (float precio){
+this.precio = precio;
+}
+
+String getNombre(){
+return this.nombre;
+}float getPrecio(){
+return this.precio;
+}
+
+public float precioIva(){
+return this.precio + this.iva * this.precio / 100;
+}
+}
+
+
+
+
+
+
+
+public class NewMain {
+
+  
+    public static void main(String[] args) {
+     
+        alimentacion leche = new alimentacion();
+        System.out.println(leche.precioIva());
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
